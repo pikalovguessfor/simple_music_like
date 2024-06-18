22644784481209e3a4a2f8d4aeb87001ea79f7fb
pikalovguessfor <pikalovvova1303@gmail.com>
2024-06-18 03:02:34 +0300

    V.3 main.cpp

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@ public:
 	}
 };
 
+
 int main() {
 	program prog;
 
@@ -56,93 +57,94 @@ int main() {
 		cout << endl;
 
 		while (prog.start == true) {
-
 			// main algoritm for gen a symbol.
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					f_line[0] = '@';
+			if (prog.start == true) {
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						f_line[0] = '@';
+					}
+					else {
+						f_line[0] = ' ';
+					}
 				}
-				else {
-					f_line[0] = ' ';
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						f_line[1] = '@';
+					}
+					else {
+						f_line[1] = ' ';
+					}
 				}
-			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					f_line[1] = '@';
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						f_line[2] = '@';
+					}
+					else {
+						f_line[2] = ' ';
+					}
 				}
-				else {
-					f_line[1] = ' ';
-				}
-			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					f_line[2] = '@';
-				}
-				else {
-					f_line[2] = ' ';
-				}
-			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					s_line[0] = '@';
-				}
-				else {
-					s_line[0] = ' ';
-				}
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						s_line[0] = '@';
+					}
+					else {
+						s_line[0] = ' ';
+					}
 
-			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					s_line[1] = '@';
 				}
-				else {
-					s_line[1] = ' ';
-				}
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						s_line[1] = '@';
+					}
+					else {
+						s_line[1] = ' ';
+					}
 
+				}
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						s_line[2] = '@';
+					}
+					else {
+						s_line[2] = ' ';
+					}
+				}
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						t_line[0] = '@';
+					}
+					else {
+						t_line[0] = ' ';
+					}
+				}
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						t_line[1] = '@';
+					}
+					else {
+						t_line[1] = ' ';
+					}
+				}
+				for (int i = 0; i < 10; i++) {
+					get_rand = (rand() % 500);
+					if (get_rand > 250) {
+						t_line[2] = '@';
+					}
+					else {
+						t_line[2] = ' ';
+					}
+				}
+				break;
+				//
 			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					s_line[2] = '@';
-				}
-				else {
-					s_line[2] = ' ';
-				}
-			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					t_line[0] = '@';
-				}
-				else {
-					t_line[0] = ' ';
-				}
-			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					t_line[1] = '@';
-				}
-				else {
-					t_line[1] = ' ';
-				}
-			}
-			for (int i = 0; i < 10; i++) {
-				get_rand = (rand() % 500);
-				if (get_rand > 250) {
-					t_line[2] = '@';
-				}
-				else {
-					t_line[2] = ' ';
-				}
-			}
-			//
-			break;
 		}
 
 		cout << endl;
@@ -152,51 +154,55 @@ int main() {
 
 		if (prog.with_music == true) {
 			// Equal trying algo.
-			if (f_line[0] == '@' && f_line[1] == '@' && f_line[2] == '@') {
-				first_line_ch = true;
-				if (first_line_ch == true) {
-					Beep(1500, 1000);
+			if (prog.with_music == true) {
+				if (f_line[0] == '@' && f_line[1] == '@' && f_line[2] == '@') {
+					first_line_ch = true;
+					if (first_line_ch == true) {
+						Beep(1500, 1000);
+					}
 				}
-			}
-			else if (s_line[0] == '@' && s_line[1] == '@' && s_line[2] == '@') {
-				second_line_ch = true;
-				if (second_line_ch == true) {
-					Beep(1900, 1000);
+				else if (s_line[0] == '@' && s_line[1] == '@' && s_line[2] == '@') {
+					second_line_ch = true;
+					if (second_line_ch == true) {
+						Beep(1900, 1000);
+					}
 				}
-			}
-			else if (t_line[0] == '@' && t_line[1] == '@' && t_line[2] == '@') {
-				third_line_ch = true;
-				if (second_line_ch == true) {
-					Beep(1700, 1000);
+				else if (t_line[0] == '@' && t_line[1] == '@' && t_line[2] == '@') {
+					third_line_ch = true;
+					if (second_line_ch == true) {
+						Beep(1700, 1000);
+					}
 				}
 			}
 			// Sound output algo
-			if (f_line[0] == '@') {
-				Beep(1500, 300);
-			}
-			else if (f_line[1] == '@') {
-				Beep(1800, 300);
-			}
-			else if (f_line[2] == '@') {
-				Beep(1000, 300);
-			}
-			if (s_line[0] == '@') {
-				Beep(2000, 300);
-			}
-			else if (s_line[1] == '@') {
-				Beep(1200, 300);
-			}
-			else if (s_line[2] == '@') {
-				Beep(1300, 300);
-			}
-			if (t_line[0] == '@') {
-				Beep(1900, 300);
-			}
-			else if (t_line[1] == '@') {
-				Beep(2100, 300);
-			}
-			else if (t_line[2] == '@') {
-				Beep(2200, 300);
+			if (prog.with_music == true) {
+				if (f_line[0] == '@') {
+					Beep(1500, 300);
+				}
+				else if (f_line[1] == '@') {
+					Beep(1800, 300);
+				}
+				else if (f_line[2] == '@') {
+					Beep(1000, 300);
+				}
+				if (s_line[0] == '@') {
+					Beep(2000, 300);
+				}
+				else if (s_line[1] == '@') {
+					Beep(1200, 300);
+				}
+				else if (s_line[2] == '@') {
+					Beep(1300, 300);
+				}
+				if (t_line[0] == '@') {
+					Beep(1900, 300);
+				}
+				else if (t_line[1] == '@') {
+					Beep(2100, 300);
+				}
+				else if (t_line[2] == '@') {
+					Beep(2200, 300);
+				}
 			}
 			//
 		}
@@ -211,25 +217,34 @@ int main() {
 			cin >> cont_or_break;
 			if (cont_or_break == 1) {
 				prog.start = true;
+				continue;
 			}
 			else if (cont_or_break == 2) {
 				prog.start = false;
+				break;
 			}
 		}
 
 		// choosing of speed console updates
-		if (prog.choose_tact == 1) {
-			Sleep(200);
-		}
-		else if (prog.choose_tact == 2) {
-			Sleep(500);
-		}
-		else if (prog.choose_tact == 3) {
-			Sleep(1000);
+		if (prog.start == true) {
+			if (prog.choose_tact == 1) {
+				Sleep(200);
+			}
+			else if (prog.choose_tact == 2) {
+				Sleep(500);
+			}
+			else if (prog.choose_tact == 3) {
+				Sleep(1000);
+			}
 		}
 		//
 
 		system("cls");
 	}
+
+	if (prog.start == false) {
+		cout << endl;
+		cout << "you choose end option...";
+	}
+
 	return 0;
-}
